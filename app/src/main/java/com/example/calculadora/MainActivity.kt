f02327374de5122d0ceb608f4f5dd1acb72efa4b
JSPA <per22318@uvg.edu.gt>
2023-08-06 18:54:55 -0600

    actualizaciones

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var resultado:TextView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resultado=findViewById(R.id.resultado)
     }
+    fun
 }
