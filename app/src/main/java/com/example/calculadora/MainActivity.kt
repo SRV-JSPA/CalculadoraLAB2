93f0affdfb2ff6b03c32b699af79da955714879b
JSPA <per22318@uvg.edu.gt>
2023-08-08 21:11:59 -0600

    actualizaciones

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -14,112 +14,180 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.Button
+import org.w3c.dom.Text
 import java.lang.Exception
 import java.util.Stack
 
 
 class MainActivity : AppCompatActivity() {
-    var resultado: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        resultado = findViewById(R.id.resultado)
-    }
+        var textView = findViewById<TextView>(R.id.resultado)
 
-    fun calcular(view: View) {
-        var botton = view as Button
-        var textobutton = botton.text.toString()
-        var concatenar = resultado?.text.toString() + textobutton
-        var imprimir = limpiarCeros(concatenar)
-        if (textobutton == "=") {
-            var resp = 0.0
 
-        } else if (textobutton == "C") {
-
+        var num0: Button = findViewById(R.id.num0)
+        var num1: Button = findViewById(R.id.num1)
+        var num2: Button = findViewById(R.id.num2)
+        var num3: Button = findViewById(R.id.num3)
+        var num4: Button = findViewById(R.id.num4)
+        var num5: Button = findViewById(R.id.num5)
+        var num6: Button = findViewById(R.id.num6)
+        var num7: Button = findViewById(R.id.num7)
+        var num8: Button = findViewById(R.id.num8)
+        var num9: Button = findViewById(R.id.num9)
+        var suma: Button = findViewById(R.id.suma)
+        var resta: Button = findViewById(R.id.resta)
+        var multiplicacion: Button = findViewById(R.id.multiplicacion)
+        var division: Button = findViewById(R.id.division)
+        var modulo: Button = findViewById(R.id.modulo)
+        var potencia: Button = findViewById(R.id.potencia)
+        var parentesisA: Button = findViewById(R.id.parentesisA)
+        var parentesisB: Button = findViewById(R.id.parentesisB)
+        var igual: Button = findViewById(R.id.igual)
+        num1.setOnClickListener() {
+            calcular(num1)
         }
-    }
+        num2.setOnClickListener() {
+            calcular(num2)
+        }
+        num3.setOnClickListener() {
+            calcular(num3)
+        }
+        num4.setOnClickListener() {
+            calcular(num4)
+        }
+        num5.setOnClickListener() {
+            calcular(num5)
+        }
+        num6.setOnClickListener() {
+            calcular(num6)
+        }
+        num7.setOnClickListener() {
+            calcular(num7)
+        }
+        num8.setOnClickListener() {
+            calcular(num8)
+        }
+        num9.setOnClickListener() {
+            calcular(num9)
+        }
+        suma.setOnClickListener() {
+            calcular(suma)
+        }
+        num0.setOnClickListener() {
+            calcular(num0)
+        }
+        resta.setOnClickListener() {
+            calcular(resta)
+        }
+        multiplicacion.setOnClickListener() {
+            calcular(multiplicacion)
+        }
+        division.setOnClickListener() {
+            calcular(division)
+        }
+        modulo.setOnClickListener() {
+            calcular(modulo)
+        }
+        potencia.setOnClickListener() {
+            calcular(potencia)
+        }
+        parentesisA.setOnClickListener() {
+            calcular(parentesisA)
+        }
+        parentesisB.setOnClickListener() {
+            calcular(parentesisB)
+        }
+        igual.setOnClickListener() {
 
-    fun limpiarCeros(str: String): String {
-        var i = 0
-        while (i < str.length && str[i] == '0') i++
-        var bs = StringBuffer(str)
-        bs.replace(0, i, "")
-        return bs.toString()
-    }
-
-    fun evaluar(str: String): Int {
-        val operands = Stack<Int>()
-        val operators = Stack<Char>()
-
-        val precedence = mapOf('+' to 1, '-' to 1, '*' to 2, '/' to 2)
-
-        fun applyOperator() {
-            val operator = operators.pop()
-            val operand2 = operands.pop()
-            val operand1 = operands.pop()
-            when (operator) {
-                '+' -> operands.push(operand1 + operand2)
-                '-' -> operands.push(operand1 - operand2)
-                '*' -> operands.push(operand1 * operand2)
-                '/' -> operands.push(operand1 / operand2)
-            }
+            var numero =  evaluar(textView.text as String)
+            textView.text = "$numero"
         }
 
-        val tokens = str.split(" ")
+
+
+
+
+
+    }
+
+
+
+    fun calcular(boton: Button){
+        boton.setOnClickListener{
+            var textView = findViewById<TextView>(R.id.resultado)
+            var texto = boton.text.toString()
+            var textoActual = textView.text.toString()
+            var textoFinal = "$textoActual $texto"
+            textView.text = textoFinal
+        }
+        //var concatenar = resultado?.text.toString() + textobutton
+
+
+    }
+
+    fun evaluar(str: String): Double {
+        val tokens = str.split(" ").filter { it.isNotEmpty() }
+        val numeros = Stack<Double>()
+        val operadores = Stack<String>()
+
+        val precedencia = mapOf("+" to 1, "-" to 1, "*" to 2, "/" to 2, "^" to 3)
+
         for (token in tokens) {
-            when {
-                token.toIntOrNull() != null -> operands.push(token.toInt())
-                token[0] in "+-*/" -> {
-                    while (operators.isNotEmpty() && operators.peek() != '(' &&
-                        precedence[token[0]]!! <= precedence[operators.peek()]!!
-                    ) {
-                        applyOperator()
-                    }
-                    operators.push(token[0])
+            if (token.toDoubleOrNull() != null) {
+                numeros.push(token.toDouble())
+            } else if (token == "(") {
+                operadores.push(token)
+            } else if (token == ")") {
+                while (operadores.peek() != "(") {
+                    val op = operadores.pop()
+                    val num2 = numeros.pop()
+                    val num1 = numeros.pop()
+                    val resultado = calculadora(op, num1, num2)
+                    numeros.push(resultado)
                 }
-
-                token == "(" -> operators.push(token[0])
-                token == ")" -> {
-                    while (operators.isNotEmpty() && operators.peek() != '(') {
-                        applyOperator()
-                    }
-                    operators.pop()
+                operadores.pop() // Pop "("
+            } else {
+                while (!operadores.isEmpty() && operadores.peek() != "(" && precedencia.getValue(token) <= precedencia.getValue(operadores.peek())) {
+                    val op = operadores.pop()
+                    val num2 = numeros.pop()
+                    val num1 = numeros.pop()
+                    val resultado = calculadora(op, num1, num2)
+                    numeros.push(resultado)
                 }
+                operadores.push(token)
             }
         }
 
-        while (operators.isNotEmpty()) {
-            applyOperator()
+        while (!operadores.isEmpty()) {
+            val op = operadores.pop()
+            val num2 = numeros.pop()
+            val num1 = numeros.pop()
+            val resultado = calculadora(op, num1, num2)
+            numeros.push(resultado)
         }
 
-        return operands.pop()
+        return numeros.pop()
     }
 
 
-    fun tipo(ch: String): Int {
-        return when (ch) {
-            "+" -> 1
-            "-" -> 1
-            "*" -> 2
-            "/" -> 2
-            "%" -> 2
-            "^" -> 3
-            else -> 0
-        }
 
-        fun calculadora(a: Int, b: Int, operador: Char): Int {
+
+        fun calculadora(operador: String, a: Double, b: Double ): Double {
             return when (operador) {
-                '+' -> a + b
-                '-' -> a - b
-                '*' -> a * b
-                '/' -> a / b
-                '%' -> a % b
-                '^' -> a * a
+                "+" -> a + b
+                "-" -> a - b
+                "*" -> a * b
+                "/" -> a / b
+                "%" -> a % b
+                "^" -> a * a
                 else -> throw IllegalArgumentException("El operador/signo no es válido ")
             }
 
-            var num1: Button = findViewById(R.id.num1)
+            /*var num1: Button = findViewById(R.id.num1)
             var num2: Button = findViewById(R.id.num2)
             var num3: Button = findViewById(R.id.num3)
             var num4: Button = findViewById(R.id.num4)
@@ -210,6 +278,6 @@ class MainActivity : AppCompatActivity() {
                 calcular(igual)
             }
 
-        }
+        }*/
     }
 }
