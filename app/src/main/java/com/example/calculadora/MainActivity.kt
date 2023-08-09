7f1a3c59a93ae66661fe4325c1319fec8756972b
JSPA <per22318@uvg.edu.gt>
2023-08-08 21:14:58 -0600

    actualizaciones

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -11,11 +11,8 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
 import android.widget.Button
-import org.w3c.dom.Text
-import java.lang.Exception
 import java.util.Stack
 
 
@@ -130,13 +127,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun evaluar(str: String): Double {
-        val tokens = str.split(" ").filter { it.isNotEmpty() }
+        val expresion = str.split(" ").filter { it.isNotEmpty() }
         val numeros = Stack<Double>()
         val operadores = Stack<String>()
 
-        val precedencia = mapOf("+" to 1, "-" to 1, "*" to 2, "/" to 2, "^" to 3)
+        val ordenOP = mapOf("+" to 1, "-" to 1, "*" to 2, "/" to 2, "^" to 3)
 
-        for (token in tokens) {
+        for (token in expresion) {
             if (token.toDoubleOrNull() != null) {
                 numeros.push(token.toDouble())
             } else if (token == "(") {
@@ -146,17 +143,17 @@ class MainActivity : AppCompatActivity() {
                     val op = operadores.pop()
                     val num2 = numeros.pop()
                     val num1 = numeros.pop()
-                    val resultado = calculadora(op, num1, num2)
-                    numeros.push(resultado)
+                    val resp = calculadora(op, num1, num2)
+                    numeros.push(resp)
                 }
                 operadores.pop() // Pop "("
             } else {
-                while (!operadores.isEmpty() && operadores.peek() != "(" && precedencia.getValue(token) <= precedencia.getValue(operadores.peek())) {
+                while (!operadores.isEmpty() && operadores.peek() != "(" && ordenOP.getValue(token) <= ordenOP.getValue(operadores.peek())) {
                     val op = operadores.pop()
                     val num2 = numeros.pop()
                     val num1 = numeros.pop()
-                    val resultado = calculadora(op, num1, num2)
-                    numeros.push(resultado)
+                    val resp = calculadora(op, num1, num2)
+                    numeros.push(resp)
                 }
                 operadores.push(token)
             }
@@ -166,8 +163,8 @@ class MainActivity : AppCompatActivity() {
             val op = operadores.pop()
             val num2 = numeros.pop()
             val num1 = numeros.pop()
-            val resultado = calculadora(op, num1, num2)
-            numeros.push(resultado)
+            val resp = calculadora(op, num1, num2)
+            numeros.push(resp)
         }
 
         return numeros.pop()
@@ -187,97 +184,6 @@ class MainActivity : AppCompatActivity() {
                 else -> throw IllegalArgumentException("El operador/signo no es válido ")
             }
 
-            /*var num1: Button = findViewById(R.id.num1)
-            var num2: Button = findViewById(R.id.num2)
-            var num3: Button = findViewById(R.id.num3)
-            var num4: Button = findViewById(R.id.num4)
-            var num5: Button = findViewById(R.id.num5)
-            var num6: Button = findViewById(R.id.num6)
-            var num7: Button = findViewById(R.id.num7)
-            var num8: Button = findViewById(R.id.num8)
-            var num9: Button = findViewById(R.id.num9)
-            var suma: Button = findViewById(R.id.suma)
-            var num0: Button = findViewById(R.id.num0)
-            var resta: Button = findViewById(R.id.resta)
-            var multiplicacion: Button = findViewById(R.id.multiplicacion)
-            var division: Button = findViewById(R.id.division)
-            var modulo: Button = findViewById(R.id.modulo)
-            var potencia: Button = findViewById(R.id.potencia)
-            var limpiar: Button = findViewById(R.id.limpiar)
-            var back: Button = findViewById(R.id.back)
-            var parentesisA: Button = findViewById(R.id.parentesisA)
-            var parentesisB: Button = findViewById(R.id.parentesisB)
-            var ans: Button = findViewById(R.id.ans)
-            var igual: Button = findViewById(R.id.igual)
-
-
-            num1.setOnClickListener() {
-                calcular(num1)
-            }
-            num2.setOnClickListener() {
-                calcular(num2)
-            }
-            num3.setOnClickListener() {
-                calcular(num3)
-            }
-            num4.setOnClickListener() {
-                calcular(num4)
-            }
-            num5.setOnClickListener() {
-                calcular(num5)
-            }
-            num6.setOnClickListener() {
-                calcular(num6)
-            }
-            num7.setOnClickListener() {
-                calcular(num7)
-            }
-            num8.setOnClickListener() {
-                calcular(num8)
-            }
-            num9.setOnClickListener() {
-                calcular(num9)
-            }
-            suma.setOnClickListener() {
-                calcular(suma)
-            }
-            num0.setOnClickListener() {
-                calcular(num0)
-            }
-            resta.setOnClickListener() {
-                calcular(resta)
-            }
-            multiplicacion.setOnClickListener() {
-                calcular(multiplicacion)
-            }
-            division.setOnClickListener() {
-                calcular(division)
-            }
-            modulo.setOnClickListener() {
-                calcular(modulo)
-            }
-            potencia.setOnClickListener() {
-                calcular(potencia)
-            }
-            limpiar.setOnClickListener() {
-                calcular(limpiar)
-            }
-            back.setOnClickListener() {
-                calcular(back)
-            }
-            parentesisA.setOnClickListener() {
-                calcular(parentesisA)
-            }
-            parentesisB.setOnClickListener() {
-                calcular(parentesisB)
-            }
-            ans.setOnClickListener() {
-                calcular(ans)
-            }
-            igual.setOnClickListener() {
-                calcular(igual)
-            }
-
-        }*/
+           
     }
 }
